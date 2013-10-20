a71576978c25cb4ae76564c280a02c646608a5de
Curtis McEnroe <programble@gmail.com>
2013-10-20 18:55:28 -0400

    Add Tokenizer

---
+++ lib/dirt/tokenizer.rb
@@ -0,0 +1,58 @@
+# Based on https://github.com/github/linguist/blob/master/lib/linguist/tokenizer.rb
+require 'strscan'
+
+module Dirt
+  class Tokenizer < StringScanner
+    def tokenize
+      tokens = Array.new
+      until eos?
+        if token = scan_token
+          tokens << token
+        else
+          getch
+        end
+      end
+      tokens
+    end
+
+    def scan_token
+      if shebang = scan_shebang
+        return shebang
+      end
+
+      # Skip comments
+      scan(%r"\s*(//|#).*") # Line
+      scan(%r"/\*.*\*/"m)   # Java/C
+      scan(/<!--.*-->/m)    # XML/HTML
+      scan(/{-.*-}/m)       # Haskell
+      scan(/\(\*.*\*\)/m)   # Coq/SML
+      scan(/""".*"""/m)     # Python
+
+      # Skip strings
+      scan(/".*[^\\]"/)
+      scan(/'.*[^\\]'/)
+
+      # Skip numbers
+      scan(/0x\h+/)
+      scan(/\d+(\.\d*)?/)
+
+      return scan(/[.:;{}()\[\]]/) || # Punctuation
+        scan(/[\w@$][\w!?'-]*/) ||    # Regular tokens
+        scan(/[=<>+*\/%^&|!-]+/)      # Operators
+    end
+
+    # Removes /usr/bin/env and trailing number (e.g. python3 -> python)
+    def scan_shebang
+      if shebang = scan(/^#!.+$/)
+        parts = shebang.split(' ')
+        script = parts.first.split('/').last
+        script = parts[1] if script == 'env' && parts[1]
+        '#!' + script[/\D+/]
+      end
+    end
+  end
+end
+
+if __FILE__ == $0
+  puts Dirt::Tokenizer.new(ARGF.read).tokenize.join(' ')
+end
---
+++ spec/tokenizer_spec.rb
@@ -0,0 +1,68 @@
+require 'dirt/tokenizer'
+
+describe Dirt::Tokenizer do
+  def scan_shebang(s)
+    described_class.new(s).scan_shebang
+  end
+
+  def tokenize(s)
+    described_class.new(s).tokenize
+  end
+
+  it 'scans shebangs' do
+    scan_shebang('#!/bin/bash').should == '#!bash'
+    scan_shebang('#!/usr/bin/ruby').should == '#!ruby'
+    scan_shebang('#!/usr/bin/env ruby').should == '#!ruby'
+    scan_shebang('#!/usr/bin/python3').should == '#!python'
+    scan_shebang('#!/usr/bin/env python3').should == '#!python'
+  end
+
+  it 'tokenizes shebangs' do
+    tokenize('#!/usr/bin/env ruby').should == ['#!ruby']
+  end
+
+  it 'skips whitespace' do
+    tokenize(" \t\n ").should == []
+  end
+
+  it 'skips line comments' do
+    tokenize('# foo').should == []
+    tokenize('// foo').should == []
+  end
+
+  it 'skips block comments' do
+    tokenize("/* foo\nbar */").should == []
+    tokenize("<!-- foo\nbar -->").should == []
+    tokenize("{- foo\nbar -}").should == []
+    tokenize("(* foo\nbar *)").should == []
+    tokenize(%Q["""foo\nbar"""]).should == []
+  end
+
+  it 'skips strings' do
+    tokenize(%q["" '' "foo" 'bar' "\"" '\'']).should == []
+  end
+
+  it 'skips numbers' do
+    tokenize('0xFF 1 1.0 1.').should == []
+  end
+
+  it 'tokenizes regular tokens' do
+    tokenize('foo bar baz').should == %w[foo bar baz]
+    tokenize("foo! foo? foo'").should == %w[foo! foo? foo']
+    tokenize('foo_bar foo-bar').should == %w[foo_bar foo-bar]
+    tokenize('@foo $foo').should == %w[@foo $foo]
+  end
+
+  it 'tokenizes punctuation' do
+    tokenize('->.:;{}()[]').should == %w"-> . : ; { } ( ) [ ]"
+  end
+
+  it 'tokenizes operators' do
+    tokenize('= == != =/=').should == %w[= == != =/=]
+    tokenize('++ -- += -= *= /= %= =/=').should == %w[++ -- += -= *= /= %= =/=]
+    tokenize('> < >= <=').should == %w[> < >= <=]
+    tokenize('+ - * / %').should == %w[+ - * / %]
+    tokenize('>> << ^ & |').should == %w[>> << ^ & |]
+    tokenize('! && ||').should == %w[! && ||]
+  end
+end
