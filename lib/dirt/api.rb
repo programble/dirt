bb74ac560f61d04ffcd63ad8fc133f1c5530aa9f
Curtis McEnroe <programble@gmail.com>
2013-10-22 19:47:54 -0400

    Add /api/detect endpoint

--- lib/dirt/api.rb
+++ lib/dirt/api.rb
@@ -7,5 +7,20 @@ require 'dirt/classifier'
 
 module Dirt
   class API < Sinatra::Base
+    def redis
+      @redis ||= Redis.new
+    end
+
+    post '/api/detect' do
+      halt 400, 'Empty sample' if params[:sample].nil? || params[:sample].empty?
+
+      tokenizer = Tokenizer.new(params[:sample])
+      classifier = Classifier.new(redis)
+
+      scores = classifier.classify(tokenizer.tokenize)
+
+      content_type :json
+      scores.sort_by {|l, s| -s }.map {|l, s| l }.to_json
+    end
   end
 end
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -6,4 +6,21 @@ describe Dirt::API do
   def app
     described_class
   end
+
+  context 'detect' do
+    it 'returns bad request for empty sample' do
+      post '/api/detect'
+      last_response.status.should == 400
+
+      post '/api/detect', sample: ''
+      last_response.status.should == 400
+    end
+
+    it 'returns a JSON array' do
+      post '/api/detect', sample: 'foo'
+
+      last_response['content-type'].should start_with('application/json')
+      JSON.parse(last_response.body).should be_an(Array)
+    end
+  end
 end
