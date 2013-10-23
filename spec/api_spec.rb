a04fc184f07ad95fa6a4a29eccc7a58bd3823675
Curtis McEnroe <programble@gmail.com>
2013-10-22 20:44:53 -0400

    Fix API spec, part three

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -7,22 +7,22 @@ describe Dirt::API do
     described_class
   end
 
-  ['detect', 'scores'].each do |endpoint|
-    context endpoint do
-      it 'returns bad request for empty sample' do
-        post "/api/#{endpoint}"
-        last_response.status.should == 400
+  it 'returns bad request for empty sample' do
+    ['detect', 'scores'].each do |endpoint|
+      post "/api/#{endpoint}"
+      last_response.status.should == 400
 
-        post "/api/#{endpoint}", sample: ''
-        last_response.status.should == 400
-      end
+      post "/api/#{endpoint}", sample: ''
+      last_response.status.should == 400
+    end
+  end
 
-      it 'returns a JSON array' do
-        post "/api/#{endpoint}", sample: 'foo'
+  it 'returns a JSON array' do
+    ['detect', 'scores'].each do |endpoint|
+      post "/api/#{endpoint}", sample: 'foo'
 
-        last_response['content-type'].should start_with('application/json')
-        JSON.parse(last_response.body).should be_an(Array)
-      end
+      last_response['content-type'].should start_with('application/json')
+      JSON.parse(last_response.body).should be_an(Array)
     end
   end
 end
