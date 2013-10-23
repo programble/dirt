5a7c7d91144ec4b2fcd455304deb7697c0146f26
Curtis McEnroe <programble@gmail.com>
2013-10-23 16:48:50 -0400

    Add limiting to API

--- lib/dirt/api.rb
+++ lib/dirt/api.rb
@@ -11,6 +11,12 @@ module Dirt
       @redis ||= Redis.new
     end
 
+    def int_param(key, default)
+      params[key] ? Integer(params[key]).tap {|i| raise if i < 0 } : default
+    rescue
+      halt 400, "Invalid #{key}"
+    end
+
     def sample_scores(params)
       halt 400, 'No sample' unless params[:sample]
 
@@ -21,18 +27,31 @@ module Dirt
     end
 
     post '/api/detect' do
-      scores = sample_scores(params)
+      limit = int_param(:limit, 10)
+
+      array = sample_scores(params).sort_by {|l, s| -s }.map {|l, s| l }
 
       content_type :json
-      scores.sort_by {|l, s| -s }.map {|l, s| l }.to_json
+      if limit == 0
+        array.to_json
+      else
+        array.take(limit).to_json
+      end
     end
 
     post '/api/scores' do
+      limit = int_param(:limit, 10)
+
       scores = sample_scores(params)
       max = -1 / scores.values.max
+      array = scores.sort_by {|l, s| -s }.map {|l, s| [l, -1 / s / max] }
 
       content_type :json
-      scores.sort_by {|l, s| -s }.map {|l, s| [l, -1 / s / max] }.to_json
+      if limit == 0
+        array.to_json
+      else
+        array.take(limit).to_json
+      end
     end
   end
 end
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -19,42 +19,78 @@ describe Dirt::API do
     ('A'..'Z').each {|l| classifier.train!(l, %w[foo bar baz]) }
   end
 
+  def detect(*args)
+    post('/api/detect', *args)
+  end
+
+  def scores(*args)
+    post('/api/scores', *args)
+  end
+
   context 'detect' do
-    def detect(*args)
-      post('/api/detect', *args)
-    end
-
-    it 'returns bad request for missing sample' do
-      detect
-      last_response.status.should == 400
-    end
-
-    it 'returns a JSON array' do
+    it 'returns a JSON array of strings' do
       detect(sample: 'foo')
-
       last_response.should be_ok
       last_response['content-type'].should start_with('application/json')
       json_body.should be_an(Array)
+      json_body.each {|s| s.should be_a(String) }
     end
   end
 
   context 'scores' do
-    def scores(*args)
-      post('/api/scores', *args)
+    it 'returns a JSON array of string-float pairs' do
+      scores(sample: 'foo')
+      last_response.should be_ok
+      last_response['content-type'].should start_with('application/json')
+      json_body.should be_an(Array)
+      json_body.each do |pair|
+        pair.should be_an(Array)
+        pair.length.should == 2
+        pair[0].should be_a(String)
+        pair[1].should be_a(Float)
+      end
     end
+  end
 
+  context 'detect/scores' do
     it 'returns bad request for missing sample' do
+      detect
+      last_response.status.should == 400
       scores
       last_response.status.should == 400
     end
 
-    it 'returns a JSON array of arrays' do
-      scores(sample: 'foo')
+    it 'returns bad request for invalid limit' do
+      detect(sample: 'foo', limit: 'bar')
+      last_response.status.should == 400
+      scores(sample: 'foo', limit: 'bar')
+      last_response.status.should == 400
 
-      last_response.should be_ok
-      last_response['content-type'].should start_with('application/json')
-      json_body.should be_an(Array)
-      json_body.each {|a| a.should be_an(Array) }
+      detect(sample: 'foo', limit: -1)
+      last_response.status.should == 400
+      scores(sample: 'foo', limit: -1)
+      last_response.status.should == 400
+    end
+
+    it 'defaults to limit of 10' do
+      detect(sample: 'foo')
+      json_body.length.should == 10
+      scores(sample: 'foo')
+      json_body.length.should == 10
+    end
+
+    it 'limits' do
+      detect(sample: 'foo', limit: 5)
+      json_body.length.should == 5
+      scores(sample: 'foo', limit: 5)
+      json_body.length.should == 5
+    end
+
+    it 'returns all results with limit of 0' do
+      detect(sample: 'foo', limit: 0)
+      json_body.length.should == 26
+      scores(sample: 'foo', limit: 0)
+      json_body.length.should == 26
     end
   end
 end
