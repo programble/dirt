3708b866fde4ffc771805ae77046479f98f3d91b
Curtis McEnroe <programble@gmail.com>
2013-10-24 21:01:16 -0400

    Implement /api/classify/raw

--- lib/dirt/api/classify.rb
+++ lib/dirt/api/classify.rb
@@ -1,8 +1,25 @@
+require 'dirt/tokenizer'
+require 'dirt/classifier'
+
+require 'json'
 require 'sinatra/base'
 
 module Dirt
   module API
     class Classify < Sinatra::Base
+      def classify(params)
+        halt 400, 'No sample' unless params[:sample]
+
+        tokenizer = Tokenizer.new(params[:sample])
+        classifier = Classifier.new
+
+        classifier.classify(tokenizer.tokenize)
+      end
+
+      post '/api/classify/raw' do
+        content_type :json
+        classify(params).to_json
+      end
     end
   end
 end
