2862b94e68cee101a672445ac77da167a0f44015
Curtis McEnroe <programble@gmail.com>
2013-10-24 21:10:03 -0400

    Implement /api/classify/scores

--- lib/dirt/api/classify.rb
+++ lib/dirt/api/classify.rb
@@ -21,6 +21,14 @@ module Dirt
         classify(params).sort_by {|l, s| -s }.map {|l, s| l }.to_json
       end
 
+      post '/api/classify/scores' do
+        raw = classify(params)
+        max = -1 / raw.values.max
+
+        content_type :json
+        raw.sort_by {|l, s| -s }.map {|l, s| [l, -1 / s / max] }.to_json
+      end
+
       post '/api/classify/raw' do
         content_type :json
         classify(params).to_json
