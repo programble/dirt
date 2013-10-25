9ef3c4b82515c7883e7bb29182293cd2932a8bc3
Curtis McEnroe <programble@gmail.com>
2013-10-24 20:55:50 -0400

    Implement /api/meta/version

--- lib/dirt/api/meta.rb
+++ lib/dirt/api/meta.rb
@@ -1,8 +1,15 @@
+require 'dirt/api'
+
+require 'json'
 require 'sinatra/base'
 
 module Dirt
   module API
     class Meta < Sinatra::Base
+      get '/api/meta/version' do
+        content_type :json
+        API::VERSION.to_json
+      end
     end
   end
 end
