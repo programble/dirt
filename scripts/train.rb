fb882ba48b8bdea156cb2684e4b725afa8944b53
Curtis McEnroe <programble@gmail.com>
2013-10-26 03:20:18 -0400

    Output arguments in train script
    [ci skip]
    [irc skip]

--- scripts/train.rb
+++ scripts/train.rb
@@ -4,4 +4,5 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'dirt/tokenizer'
 require 'dirt/classifier'
 
+puts ARGV.join(' ')
 Dirt::Classifier.new.train!(ARGV.shift, Dirt::Tokenizer.new(ARGF.read).tokenize)
