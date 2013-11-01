3e411af4b7a55ffd9f2e438d99f39b99d7d4de25
Curtis McEnroe <programble@gmail.com>
2013-10-31 21:53:40 -0400

    Alter spec for sorted set tokens

--- spec/classifier_spec.rb
+++ spec/classifier_spec.rb
@@ -18,11 +18,11 @@ describe Dirt::Classifier do
     @redis.get('tokens:total').to_i.should == 9
     @redis.get('tokens:A:total').to_i.should == 6
     @redis.get('tokens:B:total').to_i.should == 3
-    @redis.hget('tokens:A', 'foo').to_i.should == 3
-    @redis.hget('tokens:A', 'bar').to_i.should == 1
-    @redis.hget('tokens:A', 'baz').to_i.should == 2
-    @redis.hget('tokens:B', 'bar').to_i.should == 2
-    @redis.hget('tokens:B', 'baz').to_i.should == 1
+    @redis.zscore('tokens:A', 'foo').to_i.should == 3
+    @redis.zscore('tokens:A', 'bar').to_i.should == 1
+    @redis.zscore('tokens:A', 'baz').to_i.should == 2
+    @redis.zscore('tokens:B', 'bar').to_i.should == 2
+    @redis.zscore('tokens:B', 'baz').to_i.should == 1
   end
 
   it 'classifies' do
