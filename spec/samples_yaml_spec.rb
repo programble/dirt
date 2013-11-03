dbcd995794726337a634eec63219a003615e19b8
Curtis McEnroe <programble@gmail.com>
2013-11-03 17:46:12 -0500

    Add more samples.yml specs

--- spec/samples_yaml_spec.rb
+++ spec/samples_yaml_spec.rb
@@ -8,4 +8,16 @@ describe 'samples YAML file' do
   it 'is alphabetized' do
     @samples.keys.should == @samples.keys.sort
   end
+
+  it 'contains globs for each language' do
+    @samples.each do |language, hash|
+      [String, Array].should include(hash['glob'].class)
+    end
+  end
+
+  it 'contains git repositories for each language' do
+    @samples.each do |language, hash|
+      hash['git'].should be_an(Array)
+    end
+  end
 end
