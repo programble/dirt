a41e89b1d9b51d377ccb43d87ed194846540ea73
Curtis McEnroe <programble@gmail.com>
2013-11-03 18:36:35 -0500

    Use expect syntax in Meta API spec

--- spec/api/meta_spec.rb
+++ spec/api/meta_spec.rb
@@ -3,15 +3,17 @@ require 'dirt/api/meta'
 describe Dirt::API::Meta do
   include Rack::Test::Methods
 
-  context 'version' do
+  context '/version' do
     it 'returns a JSON hash' do
       get('/api/meta/version')
-      last_response.should be_ok
-      last_response['Content-Type'].should start_with('application/json')
-      json_body.should be_a(Hash)
-      json_body['string'].should be_a(String)
+
+      expect(last_response).to be_ok
+      expect(last_response['Content-Type']).to start_with('application/json')
+      expect(json_body).to be_a(Hash)
+      expect(json_body).to include('string', 'major', 'minor', 'patch')
+      expect(json_body['string']).to be_a(String)
       %w[major minor patch].each do |key|
-        json_body[key].should be_a(Fixnum)
+        expect(json_body[key]).to be_a(Integer)
       end
     end
   end
