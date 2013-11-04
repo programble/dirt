72cc4cf3ae89d81bcfcaf33dce8e60fc4d46f3cb
Curtis McEnroe <programble@gmail.com>
2013-11-03 19:55:08 -0500

    Use expect syntax in Stats API spec

--- spec/api/stats_spec.rb
+++ spec/api/stats_spec.rb
@@ -17,87 +17,106 @@ describe Dirt::API::Stats do
     end
   end
 
-  it 'stats returns a JSON stats hash' do
-    get('/api/stats')
-    last_response.should be_ok
-    json_body.should be_a(Hash)
-    json_body['languages'].should be_a(Integer)
-    json_body['samples'].should be_a(Integer)
-    json_body['tokens'].should be_a(Integer)
-  end
+  context 'stats' do
+    it 'returns a JSON stats hash' do
+      get('/api/stats')
 
-  it 'languages returns a JSON hash of stats hashes' do
-    get('/api/stats/languages')
-    last_response.should be_ok
-    json_body.should be_a(Hash)
-    json_body.each do |language, stats|
-      language.should be_a(String)
-      stats.should be_a(Hash)
-      stats['samples'].should be_a(Integer)
-      stats['tokens'].should be_a(Integer)
+      expect(last_response).to be_ok
+      expect(last_response['Content-Type']).to start_with('application/json')
+      expect(json_body).to be_a(Hash)
+      %w[languages samples tokens].each do |key|
+        expect(json_body[key]).to be_a(Integer)
+      end
     end
   end
 
-  context 'tokens' do
+  context 'stats/languages' do
+    it 'returns a JSON hash of stats hashes' do
+      get('/api/stats/languages')
+
+      expect(last_response).to be_ok
+      expect(last_response['Content-Type']).to start_with('application/json')
+      expect(json_body).to be_a(Hash)
+      json_body.each do |language, stats|
+        expect(language).to be_a(String)
+        expect(stats).to be_a(Hash)
+        expect(stats['samples']).to be_an(Integer)
+        expect(stats['tokens']).to be_an(Integer)
+      end
+    end
+  end
+
+  context 'stats/tokens' do
+    def tokens(*args)
+      get('/api/stats/tokens', *args)
+    end
+
     it 'returns 404 for unknown language' do
-      get('/api/stats/tokens')
-      last_response.status.should == 404
+      tokens
+      expect(last_response.status).to eq(404)
     end
 
     it 'returns a JSON array of pairs' do
-      get('/api/stats/tokens', language: 'A')
-      last_response.should be_ok
-      json_body.should be_an(Array)
+      tokens(language: 'A')
+
+      expect(last_response).to be_ok
+      expect(last_response['Content-Type']).to start_with('application/json')
+      expect(json_body).to be_an(Array)
       json_body.each do |pair|
-        pair.length.should == 2
-        pair[0].should be_a(String)
-        pair[1].should be_a(Integer)
+        expect(pair).to be_an(Array)
+        expect(pair.length).to eq(2)
+        expect(pair[0]).to be_a(String)
+        expect(pair[1]).to be_an(Integer)
       end
     end
 
     it 'defaults to limit 20' do
-      get('/api/stats/tokens', language: 'A')
-      json_body.length.should == 20
+      tokens(language: 'A')
+      expect(json_body.length).to eq(20)
     end
 
     it 'limits' do
-      get('/api/stats/tokens', language: 'A', limit: 10)
-      json_body.length.should == 10
+      tokens(language: 'A', limit: 10)
+      expect(json_body.length).to eq(10)
     end
 
     it 'returns 403 for limit over maximum' do
-      get('/api/stats/tokens', language: 'A', limit: 1001)
-      last_response.status.should == 403
+      tokens(language: 'A', limit: 1001)
+      expect(last_response.status).to eq(403)
     end
 
     it 'paginates' do
-      get('/api/stats/tokens', language: 'A')
+      tokens(language: 'A')
       sixth = json_body[5]
-      get('/api/stats/tokens', language: 'A', limit: 5, page: 2)
-      json_body.first.should == sixth
+      tokens(language: 'A', limit: 5, page: 2)
+      expect(json_body.first).to eq(sixth)
     end
 
     it 'returns 404 for out of bounds page' do
-      get('/api/stats/tokens', language: 'A', page: 10000)
-      last_response.status.should == 404
+      tokens(language: 'A', page: 10000)
+      expect(last_response.status).to eq(404)
     end
 
     it 'returns 400 for invalid limit' do
-      get('/api/stats/tokens', language: 'A', limit: 0)
-      last_response.status.should == 400
-      get('/api/stats/tokens', language: 'A', limit: -1)
-      last_response.status.should == 400
-      get('/api/stats/tokens', language: 'A', limit: 'a')
-      last_response.status.should == 400
+      tokens(language: 'A', limit: 0)
+      expect(last_response.status).to eq(400)
+
+      tokens(language: 'A', limit: -1)
+      expect(last_response.status).to eq(400)
+
+      tokens(language: 'A', limit: 'a')
+      expect(last_response.status).to eq(400)
     end
 
     it 'returns 400 for invalid page' do
-      get('/api/stats/tokens', language: 'A', page: 0)
-      last_response.status.should == 400
-      get('/api/stats/tokens', language: 'A', page: -1)
-      last_response.status.should == 400
-      get('/api/stats/tokens', language: 'A', page: 'a')
-      last_response.status.should == 400
+      tokens(language: 'A', page: 0)
+      expect(last_response.status).to eq(400)
+
+      tokens(language: 'A', page: -1)
+      expect(last_response.status).to eq(400)
+
+      tokens(language: 'A', page: 'a')
+      expect(last_response.status).to eq(400)
     end
   end
 end
